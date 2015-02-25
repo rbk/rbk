d737c4b3c0a3e1d25ca39f5c78fb93cfd8771e5e
Richard <richard.be.jamin@gmail.com>
2015-02-25 00:52:50 -0600

    fixed customizer

--- wp/wp-content/themes/rbk/includes/customizer_background_image.php
+++ wp/wp-content/themes/rbk/includes/customizer_background_image.php
@@ -1,18 +1,17 @@
 <?php
 
 function background_image_theme_customizer( $wp_customize ) {
-    // Fun code will go here
-	$wp_customize->add_section( 'theme_background_image_section' , array(
-	    'title'       => __( 'Background Image', 'themeslug' ),
-	    'priority'    => 1,
-	    'description' => 'Upload a background image.',
+	$wp_customize->add_section( 'background_image_section' , array(
+		'title'       => __( 'Background Image', 'themebackground' ),
+		'priority'    => 30,
+		'description' => 'Background image uploader.',
 	) );
 	$wp_customize->add_setting( 'theme_background_image' );
 	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'theme_background_image', array(
-	    'label'    => __( 'Logo', 'themeslug' ),
-	    'section'  => 'theme_background_image_section',
-	    'settings' => 'theme_background_image',
-	) ) );
+    'label'    => __( 'Background Image', 'themebackground' ),
+    'section'  => 'background_image_section',
+    'settings' => 'theme_background_image',
+) ) );
 }
 add_action('customize_register', 'background_image_theme_customizer');
 
