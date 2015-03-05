d67aaade9d24f6af78f9891fb41a7d17b41ffcdf
Richard <richard.be.jamin@gmail.com>
2015-03-04 20:44:03 -0600

    added js to fixed wpadminbar while viewing logged in

--- wp/wp-content/themes/rbk/footer.php
+++ wp/wp-content/themes/rbk/footer.php
@@ -5,11 +5,31 @@
 	</div>
 </footer>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53226484-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 </div> <!-- end #page-container -->
+
 
 <div id="page-color"></div>
 <div id="overlay"></div>
 
 <?php wp_footer(); ?>
+<script>
+	console.log(document.getElementById('wpadminbar'))	
+	if( document.getElementById('wpadminbar') ){
+		document.body.style.paddingTop = '132px';
+		document.getElementById('header').style.top = '32px';
+	}
+
+</script>
 </body>
 </html>
