3907dd61d0bc7e795db0c7482d4589f6d2de530b
Richard <richard.be.jamin@gmail.com>
2015-04-02 03:05:56 -0500

    updated stype

--- wp/wp-content/themes/rbk/header.php
+++ wp/wp-content/themes/rbk/header.php
@@ -5,7 +5,7 @@
 	<title><?php echo get_bloginfo('name'); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Droid+Serif' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
 	<style>
